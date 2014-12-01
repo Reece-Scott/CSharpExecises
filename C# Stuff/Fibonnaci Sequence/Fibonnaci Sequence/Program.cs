05bf9e925227236b18d767a6f53a1a089c3beb0b
Reece-Scott <Reece.Scott@tombola.com>
2014-12-01 15:32:24 +0000

    More Exercises
    Added Fibonnaci Sequence and FizzBuzz

---
+++ C# Stuff/Fibonnaci Sequence/Fibonnaci Sequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonnaci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int finNumber1 = 0;
+            int finNumber2 = 1;
+            int sumNumber = 1;
+            Console.WriteLine("Press any key to see 100 in a fibonnaci sequence: ");
+            Console.ReadKey();
+            while (finNumber1 < 100)
+            {
+                sumNumber = finNumber1 + finNumber2;
+                finNumber1 = finNumber2;
+                finNumber2 = sumNumber;
+                Console.WriteLine(finNumber2);
+            }
+            Console.ReadLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
--- C# Stuff/Integer Factorial/Integer Factorial/Program.cs
+++ C# Stuff/Integer Factorial/Integer Factorial/Program.cs
@@ -11,7 +11,19 @@ namespace Integer_Factorial
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the factorial calculator");
+            Console.WriteLine();
+            Console.WriteLine("Please enter a number");
+            int inputNumber = int.Parse(Console.ReadLine());
+            int factorialNumber = inputNumber;
 
+            for (i = inputNumber - 1; i >= 1; i--) 
+            {
+                factorialNumber = factorialNumber * i;
+            }
+            Console.WriteLine("{0}", factorialNumber);
+            Console.ReadKey();
         }
+
+        public static int i { get; set; }
     }
 }
