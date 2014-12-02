62293491c5242d57e8e5562b2e238d14389d39c2
Reece-Scott <Reece.Scott@tombola.com>
2014-12-02 16:51:45 +0000

    Added Palindrome Program

--- C# Stuff/Palindrome/Palindrome/Program.cs
+++ C# Stuff/Palindrome/Palindrome/Program.cs
@@ -10,9 +10,34 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
+            int userinputNumber;
+            int overallRemainder;
+            int sum = 0;
+            int temp;
+
             Console.WriteLine("Hello, welcome to the Palindrome number program");
             Console.WriteLine();
             Console.WriteLine("Please enter your number");
+            userinputNumber = Convert.ToInt32(Console.ReadLine());
+            temp = userinputNumber;
+
+            while (Convert.ToBoolean(userinputNumber))
+            {
+                overallRemainder = userinputNumber % 10;
+                userinputNumber = userinputNumber / 10;
+                sum = sum * 10 + overallRemainder;
+            }
+            Console.WriteLine("The Reversed Value Is: {0}", sum);
+
+            if (temp == sum)
+            {
+                Console.WriteLine("The Number Is A Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The Number Is Not A Palindrome");
+            }
+            Console.ReadKey();
 
         }
     }
