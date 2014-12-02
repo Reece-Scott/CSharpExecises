0919b27d2d9b485197977265ff5a19c80ec405b2
Reece-Scott <Reece.Scott@tombola.com>
2014-12-02 17:20:24 +0000

    Added Current Date + Time

---
+++ C# Stuff/Current Time/Current Time/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Current_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            Console.ReadKey();
+        }
+    }
+}
