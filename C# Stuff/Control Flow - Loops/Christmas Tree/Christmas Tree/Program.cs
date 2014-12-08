0a4c11e9534e5b47680aa56e94176315d37a38ff
Reece-Scott <Reece.Scott@tombola.com>
2014-12-08 12:00:46 +0000

    Added Christmas Tree Exercise

--- C# Stuff/Control Flow - Loops/Christmas Tree/Christmas Tree/Program.cs
+++ C# Stuff/Control Flow - Loops/Christmas Tree/Christmas Tree/Program.cs
@@ -10,6 +10,28 @@ namespace Christmas_Tree
     {
         static void Main(string[] args)
         {
+             Console.WriteLine("Welcome to the triangle program");
+            Console.ReadKey();
+
+            int triangleSize = 6;
+
+            for(int i = 1; i <= triangleSize; i++)
+            {
+                for (int k = 1; k <= triangleSize - i; k++)
+                    Console.Write(" ");
+                for (int l = 1; l <= i; l++)
+                    Console.Write(" *");
+                    Console.WriteLine("");
+            }
+                for (int counter = 0; counter < 2; counter++)
+                {
+                     Console.WriteLine("      !");
+                }
+
+              Console.ReadKey();
+
+            }
         }
-    }
-}
+     }
+  
+
