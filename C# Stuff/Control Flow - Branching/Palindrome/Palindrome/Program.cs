865f8d2ee9f8116c8b72f31941db91b29381bc70
Reece-Scott <Reece.Scott@tombola.com>
2014-12-08 11:44:18 +0000

    Fixed Palindrome
    Fixed as per Tello instructions

--- C# Stuff/Control Flow - Branching/Palindrome/Palindrome/Program.cs
+++ C# Stuff/Control Flow - Branching/Palindrome/Palindrome/Program.cs
@@ -10,25 +10,19 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            int userinputNumber;
-            int overallRemainder;
-            int sum = 0;
-            int temp;
 
             Console.WriteLine("Hello, welcome to the Palindrome number program");
             Console.WriteLine();
             Console.WriteLine("Please enter your number");
-            userinputNumber = Convert.ToInt32(Console.ReadLine());
-            temp = userinputNumber;
-
+            int userinputNumber = Convert.ToInt32(Console.ReadLine());
+            int temp = userinputNumber;
+            int sum = 0;
             while (Convert.ToBoolean(userinputNumber))
             {
-                overallRemainder = userinputNumber % 10;
+                int overallRemainder = userinputNumber % 10;
                 userinputNumber = userinputNumber / 10;
                 sum = sum * 10 + overallRemainder;
             }
-            Console.WriteLine("The Reversed Value Is: {0}", sum);
-
             if (temp == sum)
             {
                 Console.WriteLine("The Number Is A Palindrome");
