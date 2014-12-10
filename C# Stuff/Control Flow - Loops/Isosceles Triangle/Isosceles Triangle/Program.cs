ff1d9db9d90de27ca8b793bcfd8e9c8bc1926372
Reece-Scott <Reece.Scott@tombola.com>
2014-12-10 16:23:35 +0000

    Fixed Right Angle (Isosceles Triangle)
    Fixed per email

--- C# Stuff/Control Flow - Loops/Isosceles Triangle/Isosceles Triangle/Program.cs
+++ C# Stuff/Control Flow - Loops/Isosceles Triangle/Isosceles Triangle/Program.cs
@@ -12,14 +12,13 @@ namespace Isosceles_Triangle
             {
                 Console.WriteLine("Welcome to the sideways triangle program");
                 Console.WriteLine();
-                Console.WriteLine("Press enter to draw a triangle");
-                Console.ReadKey();
+                Console.WriteLine("How large is the triangle?");
+                int triangleSize = int.Parse(Console.ReadLine());
 
-                int i;
-                int k;
-                for (i = 0; i < 8; i++)
+                
+                for (int i = 0; i < triangleSize; i++)
                 {
-                    for (k = 0; k < i; k++)
+                    for (int k = 0; k < i; k++)
                         Console.Write("*");
                     Console.WriteLine();
                 }
