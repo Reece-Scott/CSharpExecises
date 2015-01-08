91003c8ab9c6a4b3976538013d7230f26df988dc
Reece-Scott <Reece.Scott@tombola.com>
2015-01-08 10:41:09 +0000

    Last Entry - Collections
    Added program

---
+++ C# Stuff/Control Flow - Loops/Last Entry/Last Entry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last_Entry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> userInputs = new List<string>();
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput.ToLowerInvariant() == "done")
+                {
+                    userInput = Console.ReadLine();
+                    userInputs.Add(userInput);
+                    break;
+                }
+                userInputs.Add(userInput);
+            }
+
+            foreach (string userInput in userInputs)
+            {
+                Console.WriteLine(userInput);
+            }
+            Console.ReadKey();
+        }
+    }
+}
