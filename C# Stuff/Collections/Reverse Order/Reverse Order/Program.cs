5a77b7f6068b4282e3aa25c14009d391816f477b
Reece-Scott <Reece.Scott@tombola.com>
2015-01-21 11:49:21 +0000

    Collections - Reverse Output
    Added Reverse Order program

---
+++ C# Stuff/Collections/Reverse Order/Reverse Order/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program takes your input, and reverses the output.");
+            Console.WriteLine("Enter characters/words, and enter 'done' when finished");
+
+            Stack<string> userStack = new Stack<string>();
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput.ToLowerInvariant() == "done")
+                {
+                    break;
+                }
+                userStack.Push(userInput);
+            }
+
+            foreach (string userInput in userStack)
+            {
+                Console.WriteLine(userInput);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
